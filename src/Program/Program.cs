0e52d19f13c7c8fb4c62893622fd00e5300fee0a
fedepds <federico.pizarro@correo.ucu.edu.uy>
2023-06-05 20:59:24 -0300

    e falto gitear program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System;
 using Library;
 
 namespace Program
@@ -7,13 +9,16 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+
+            Node n1 = new Node(new Persona("Javier",12));
+            Node n2 = new Node(new Persona("Federico",21));
+            Node n3 = new Node(new Persona("Pancho", 31));
+            Node n4 = new Node(new Persona("Cristian", 90));
+            Node n5 = new Node(new Persona("Alfo",45));
+            Node n6 = new Node(new Persona("Kira",34));
+            Node n7 = new Node(new Persona("Santi",3));
+            Node n8 = new Node(new Persona("Joaco", 15));
+
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -23,8 +28,8 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+            n2.AddChildren(n8);
 
-            // visitar el árbol aquí
         }
     }
 }
